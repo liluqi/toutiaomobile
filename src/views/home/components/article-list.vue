36e76353d5aa687402a98d8bba9458b59ae58a39
liluqi <997613532@qq.com>
2020-03-19 21:06:19 +0800

    上拉加载完成

--- src/views/home/components/article-list.vue
+++ src/views/home/components/article-list.vue
@@ -2,7 +2,7 @@
   <div class="scroll-wrapper">
       <!-- 当van-list距离底部超过一定距离,会自动开启upload为true; 当组件滚动到底部时，会触发load事件并将loading设置成true -->
       <van-list v-model="upload" :finished="finished" finished-text="没有更多了" @load="onLoad">
-        <van-cell title="标题" value="内容" :key="item" v-for="item in 20"></van-cell>
+        <van-cell title="标题" :value="item" :key="item" v-for="item in article"></van-cell>
       </van-list>
   </div>
 </template>
@@ -12,11 +12,28 @@ export default {
   data () {
     return {
       upload: false,
-      finished: false
+      finished: false,
+      article: []// 用于存放新闻信息
     }
   },
   methods: {
-    onLoad () {}
+    onLoad () {
+      // 当他没有内容的时候,加载一秒钟,然后设置加载完毕
+    //   setTimeout(() => {
+    //     this.finished = true
+    //   }, 1000)
+    // 当他有内容的时候
+      setTimeout(() => {
+        if (this.article.length > 50) {
+          this.finished = true
+        } else {
+          // 定义一个假的数据,每次执行onLoad函数都添加十条数据到article数组中
+          const arr = Array.from(Array(10), (value, index) => this.article.length + index + 1)
+          this.article.push(...arr)
+          this.upload = false
+        }
+      }, 1000)
+    }
   }
 }
 </script>
